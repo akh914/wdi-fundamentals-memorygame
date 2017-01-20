4fea79e40c7b637c2463cfa74598ceea1ca98dcd
Allison Kendall <allisonkendall6@gmail.com>
2017-01-20 18:59:37 -0500

    Add if statement to check cardTwo and card Four

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!")
+} else {
+	"Sorry, try again"
+}
